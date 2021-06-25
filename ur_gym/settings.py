434f806e28ac8af84825893f2f79f884668e3bc4
Ifti Khan <ifti_k@hotmail.com>
2021-06-25 21:36:38 +0100

    New: Home app, index.html created and settings configured

--- ur_gym/settings.py
+++ ur_gym/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
 import environ
 from pathlib import Path
 
@@ -50,6 +51,9 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+
+    # my created apps for the project
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -67,7 +71,10 @@ ROOT_URLCONF = 'ur_gym.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
