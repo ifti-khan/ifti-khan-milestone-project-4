3247ac8835c70a4528916a3ab24ee9678a280b18
Ifti Khan <ifti_k@hotmail.com>
2021-06-29 13:13:06 +0100

    New: Add base js for footer year, Mod: footer done and styled

---
+++ static/js/base.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    // This is for the copyright year located in the footer 
+    //and will auto update when the year date changes
+    $("#copyright-footerYear").text(new Date().getFullYear());
+
+});
