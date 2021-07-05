b3c5f9c9bc2e2a49e23c378bdbfc163e969496c4
Ifti Khan <ifti_k@hotmail.com>
2021-07-06 00:04:32 +0100

    Mod: Product details html, views, urls added and navigation

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
@@ -17,3 +17,18 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_details(request, product_id):
+    """
+    A view to show individual product details
+    when a product card is clicked on
+    """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_details.html', context)
