ac920c7e3204e546ce69681e779389725ba66b20
Ifti Khan <ifti_k@hotmail.com>
2021-07-11 22:47:25 +0100

    New: Template tags dir, trolley tools, init.py added

---
+++ trolley/templatetags/trolley_tools.py
@@ -0,0 +1,8 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter(name='calc_subtotal')
+def calc_subtotal(product_price, quantity):
+    return product_price * quantity
