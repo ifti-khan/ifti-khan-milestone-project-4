c4473790543e0e15b3c48a1ce26d4de9b4360bae
Ifti Khan <ifti_k@hotmail.com>
2021-07-12 20:15:24 +0100

    Mod: Code comments and position adjustments done

--- trolley/templatetags/trolley_tools.py
+++ trolley/templatetags/trolley_tools.py
@@ -2,6 +2,9 @@ from django import template
 
 register = template.Library()
 
+# Custom template tag for subtotal calculation to be used
+# within the trolley.html
+
 
 @register.filter(name='calc_subtotal')
 def calc_subtotal(product_price, quantity):
