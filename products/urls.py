9bf7cd2803d9770cdd221e3d5f377af2aaf483ed
Ifti Khan <ifti_k@hotmail.com>
2021-07-21 20:50:34 +0100

    Mod: Add produt view logic, url, template added

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,6 @@ from . import views
 # All product app URLs
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('<product_id>', views.product_details, name='product_details'),
+    path('<int:product_id>/', views.product_details, name='product_details'),
+    path('add/', views.add_product, name='add_product'),
 ]
