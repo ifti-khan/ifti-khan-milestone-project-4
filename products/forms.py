2fcebfef1457b863a8da20c2aef6f25057b8e010
Ifti Khan <ifti_k@hotmail.com>
2021-07-21 20:49:54 +0100

    New: Prorduct form added for adding new products

---
+++ products/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from .models import Product, Category
+
+
+class ProductForm(forms.ModelForm):
+
+    # dunder or __all__ means all fields will be included in the form
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    # Overriding the init method to make changes to the fields
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Getting all categories
+        categories = Category.objects.all()
+        # Creating a tuple for the category friendly names using
+        # list comprehension which is a short hand way of creating a for loop
+        category_friendly_name = [(
+            cat.id, cat.get_friendly_name()) for cat in categories]
+
+        # Updating the categories on the form to use the friendly names
+        self.fields['category'].choices = category_friendly_name
+        for field_category_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'rounded-0'
