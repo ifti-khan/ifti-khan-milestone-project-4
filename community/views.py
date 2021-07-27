28538aa7ffb950f243755ba4937dabded1a5d57a
Ifti Khan <ifti_k@hotmail.com>
2021-07-27 05:02:13 +0100

    Mod: Community question fully styled and responsive

--- community/views.py
+++ community/views.py
@@ -6,9 +6,12 @@ def community(request):
     """
     This view returns the community home page
     """
-    # Getting community questions
-    questions = CommunityQuestion.objects.all()
+    # Getting community questions and ordering by most recent
+    questions = CommunityQuestion.objects.all().order_by(
+        '-date_created', '-time_created')
 
+    # context dictionary with keys and values to be
+    # used in the rendered html template
     templates = 'community/community.html'
     context = {
         'questions': questions,
