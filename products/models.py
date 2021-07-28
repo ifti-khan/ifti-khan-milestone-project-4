a6845de65b5060f2a0ae4f124ffe0381be52665b
Ifti Khan <ifti_k@hotmail.com>
2021-07-29 00:51:49 +0100

    Fix: Removed default product sizes, non sized product showed sizes

--- products/models.py
+++ products/models.py
@@ -42,7 +42,7 @@ class Product(models.Model):
         max_digits=6, decimal_places=2, null=True, blank=True)
     sku = models.CharField(
         max_length=254, null=True, blank=True)
-    product_sizes = models.BooleanField(default=True, null=True, blank=True)
+    product_sizes = models.BooleanField(null=True, blank=True)
     product_image = models.ImageField(null=True, blank=True)
     product_image_url = models.URLField(
         max_length=1024, null=True, blank=True)
